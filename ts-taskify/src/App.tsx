78e13a752f20046431bde5321c3755f104657e3e
Clark Oake <77975185+clarkOake13@users.noreply.github.com>
2022-02-12 12:38:33 -0330

    clean up structure to start coding, also did some practice typing with ts

--- ts-taskify/src/App.tsx
+++ ts-taskify/src/App.tsx
@@ -1,24 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+// ============================================================================
+// Typing practice
+// ============================================================================
+// ===  primitives  ===
+// let name: string;
+// let age: number;
+// let is_student: boolean;
+
+// ===  unions  ===
+// let age_union: number | string;
+
+// ===  typed array  ===
+// let hobbies: string[];
+
+// ===  tuple  ===
+// let role: [number, string];
+
+// ===  ANY TYPE ===
+// let anything: any;
+
+// === UNKNOWN TYPE ===
+// let unknown_value: unknown;
+
+
+// === Objects ===
+// ==  without interface ==
+// let person_plain: Object;
+
+
+// ==  with interface  ==
+// type Person = 
+// {
+//   name: string;
+//   age: number;
+//   is_student?: boolean;
+//   hobbies?: string[];
+//   role?: [number, string];
+// }
+
+// let clark: Person = 
+// {
+//   name: "Clark",
+//   age: 30,
+//   is_student: true,
+//   hobbies: ["reading", "coding", "cooking"],
+// }
+
+// let jeffrey: Person = 
+// {
+//   name: "Jeffrey",
+//   age: 43,
+//   is_student: false,
+//   hobbies: ["hunting", "smoking", "adultery"],
+// }
+
+// let susan: Person = 
+// {
+//   name: "Susan",
+//   age: 25,
+//   is_student: true,
+//   hobbies: ["knitting", "psychology", "hiking"],
+// }
+
+// ===  Array of objects  ===
+// let lots_of_people: Person[];
+
+// lots_of_people = [clark, jeffrey, susan]
+
+
+// === Functions === 
+
+// Method 1
+let printName: Function;
+
+printName = ( name: string ) => {
+  console.log(name);
+};
+
+printName("Piyush");
+
+// Method 2
+// define argument types and return types, if no return, void
+// void returns undefined, never doesn't return anything
+let printBetterName: ( name:string ) => void
+
+printBetterName = ( name: string ) => {
+  console.log(name);
+}
+
+printBetterName("Meredith")
+
+// ============================================================================
+// end of typing pratice
+// ============================================================================
+
+
+
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
